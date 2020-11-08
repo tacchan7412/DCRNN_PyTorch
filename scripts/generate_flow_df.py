7610f5f69de3b65d469c2def0b44c0a1d2076c2e
tacchan7412 <tacchan.04@gmail.com>
2020-11-08 10:38:04 -0800

    fix flow generating script

--- scripts/generate_flow_df.py
+++ scripts/generate_flow_df.py
@@ -6,19 +6,22 @@ print("started processing")
 df = pd.read_hdf('data/pems-bay.h5')
 sensor_ids = df.columns.values
 
-for filename in sorted(os.listdir('data/pems-bay-raw')):
+raw_dir = 'data/pems-bay-raw'
+frames = []
+for filename in sorted(os.listdir(raw_dir)):
     print('processing:', filename)
-    if filename == 'd04_text_station_5min_2017_06_01.txt':
+    if filename == 'd04_text_station_5min_2017_07_01.txt':
         print('finished processing')
         break
-    with open('data/pems-bay-raw/' + filename, 'r') as f:
-        for i, line in enumerate(f.readlines()):
-            data = line.split(',')
-            time = pd.to_datetime(data[0])
-            sensor_id = int(data[1])
-            flow = float(data[9]) if data[9] != '' else 0
-            if sensor_id in sensor_ids:
-                df.at[time, sensor_id] = flow
+    df = pd.read_csv(os.path.join(raw_dir, filename), sep=',', header=None)
+    df = df.iloc[:, [0, 1, 9]]
+    df.columns = ['time', 'sensor_id', 'flow']
+    df = df[df['sensor_id'].isin(sensor_ids)]
+    df['time'] = pd.to_datetime(df['time'])
+    df.fillna(0)
+    df = df.pivot_table(values='flow', index='time', columns='sensor_id')
+    frames.append(df)
 
+final_df = pd.concat(frames)
 print('saving new dataframe')
-df.to_hdf('data/pems-bay-flow.h5')
+final_df.to_hdf('data/pems-bay-flow.h5', key='df')
