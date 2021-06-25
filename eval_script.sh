b15a35178d954f3f13f742d6d145daa08d0bc150
tacchan7412 <tacchan.04@gmail.com>
2021-06-25 14:07:02 -0700

    fix eval

--- eval_script.sh
+++ eval_script.sh
@@ -4,7 +4,7 @@ echo "don't forget to update output_filename argument"
 export CUDA_VISIBLE_DEVICES=0
 
 python run_demo_pytorch.py --config_filename data/model/pretrained/PEMS-BAY-FLOW/raw.yaml --output_dir /tmp3/tacchan7412/DCRNN_PyTorch/predictions/raw
-python run_demo_pytorch.py --config_filename data/model/pretrained/PEMS-BAY-FLOW/dft.yaml --output_dir /tmp3/tacchan7412/DCRNN_PyTorch/predictions/dft/1
-python run_demo_pytorch.py --config_filename data/model/pretrained/PEMS-BAY-FLOW/gaussian.yaml --output_dir /tmp3/tacchan7412/DCRNN_PyTorch/predictions/gaussian/1
-python run_demo_pytorch.py --config_filename data/model/pretrained/PEMS-BAY-FLOW/ss.yaml --output_dir /tmp3/tacchan7412/DCRNN_PyTorch/predictions/ss/1
-python run_demo_pytorch.py --config_filename data/model/pretrained/PEMS-BAY-FLOW/ssf.yaml --output_dir /tmp3/tacchan7412/DCRNN_PyTorch/predictions/ssf/1
+python run_demo_pytorch.py --config_filename data/model/pretrained/PEMS-BAY-FLOW/dft.yaml --output_dir /tmp3/tacchan7412/DCRNN_PyTorch/predictions/dft
+python run_demo_pytorch.py --config_filename data/model/pretrained/PEMS-BAY-FLOW/gaussian.yaml --output_dir /tmp3/tacchan7412/DCRNN_PyTorch/predictions/gaussian
+python run_demo_pytorch.py --config_filename data/model/pretrained/PEMS-BAY-FLOW/ss.yaml --output_dir /tmp3/tacchan7412/DCRNN_PyTorch/predictions/ss
+python run_demo_pytorch.py --config_filename data/model/pretrained/PEMS-BAY-FLOW/ssf.yaml --output_dir /tmp3/tacchan7412/DCRNN_PyTorch/predictions/ssf
